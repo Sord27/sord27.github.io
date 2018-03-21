92c89c423a77516fc68f31f232b169c00cca6035
Sord27 <bratsergey@i.ua>
2018-03-21 10:20:27 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+
+    var scrollTop, skillOffset;
+	var header = document.getElementById('header');
+    var skillSection = document.getElementById('skill');
+    
+	window.onscroll = function() {
+		scrollTop = window.pageYOffset;
+		skillOffset = skillSection.offsetTop - 500;
+
+		if (scrollTop >= 250) {
+			header.classList.add('fixed');
+		} else {
+			header.classList.remove('fixed');
+		}
+
+		if (scrollTop > skillOffset) {
+            skillSection.classList.add('active');
+		} else {
+            skillSection.classList.remove('active');
+ 		}
+    }
+  
+   //window onload end 
+}
